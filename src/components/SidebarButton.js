43368ee0535bc68006b176d4d6897a8ad26bc2b5
Ryu <ryu@realtime.tw>
2022-10-01 16:33:47 +0800

    fix: layout mobile guide button add shadow

--- src/components/SidebarButton.js
+++ src/components/SidebarButton.js
@@ -7,7 +7,7 @@ const SidebarButton = (props) => {
         <button
           type="button"
           className="bg-primary w-8 h-24 flex items-center justify-center
-            rounded-r-lg lg:hidden"
+            rounded-r-lg lg:hidden shadow-lg"
           onClick={() => { props.onClick(!props.toggleSidebar) }}
         >
           <span className="material-icons-outlined text-5xl text-warning">
@@ -17,7 +17,7 @@ const SidebarButton = (props) => {
       </div>
       <div className="absolute bottom-5 right-5 md:hidden">
         <div
-          className="w-16 h-16 flex items-center justify-center bg-primary rounded-full"
+          className="w-16 h-16 flex items-center justify-center bg-primary rounded-full shadow-lg"
           onClick={() => { props.onClick(false) }}
         >
           <span className="material-icons-outlined text-4xl text-warning">
